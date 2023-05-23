cda50813011aaa744af6d16f2972f5c23c3e79c3
= <=>
2023-05-23 15:15:45 -0600

    finished implementing pagination on home page

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
     try {
       const page = req.query.page || 1;
       const results = await Pet.paginate({}, { page: page });
-      res.render('pets-index', { pets: results.docs });
+      res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
     } catch (err) {
       console.log(err.message);
       res.status(500);
